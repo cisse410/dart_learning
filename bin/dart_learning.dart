cf18a07cb156ba4cd4cdf3846e52340c7c1a7284
cisse410 <dramisse90@gmail.com>
2024-05-09 00:34:16 +0000

    Future and async

--- bin/dart_learning.dart
+++ bin/dart_learning.dart
@@ -1,12 +1,37 @@
-import 'package:dart_learning/oop/bank.dart';
-import 'package:dart_learning/oop/house.dart';
+import 'dart:io';
 
 void main(List<String> arguments) async {
-  House house = House(
-      numberOfWindows: 15,
-      numberOfDoors: 3,
-      typeOfWalls: 'Plaster',
-      typeOfRoofs: 'Tile');
-  house.printData();
-  bankManagement();
+  goEatPizza();
+}
+
+void goEatPizza() async {
+  order();
+  getPizzaOrdered().then((value) {
+    eatAndPay(value);
+  });
+  chatting();
+}
+
+void order() {
+  print('Done ordering pizza');
+}
+
+Future<String> getPizzaOrdered() async {
+  Duration waitTime = Duration(seconds: 4);
+  String pizza = 'No Pizza yet';
+  await Future.delayed(waitTime, () {
+    pizza = 'Carnivore';
+    print('Pizza delivered successfully');
+  });
+  return pizza;
+}
+
+void chatting() {
+  print('Chatting.....');
+}
+
+void eatAndPay(String pizza) {
+  Duration waitTime = Duration(seconds: 5);
+  sleep(waitTime);
+  print('Paid $pizza and on my way home');
 }
