1f35e6f5fc455a32a191b59cb2f89e8b29c4d8db
cisse410 <dramisse90@gmail.com>
2024-05-09 02:01:59 +0000

    Manipulating data retrieved from the internet

--- lib/fetch_data_internet.dart
+++ lib/fetch_data_internet.dart
@@ -4,15 +4,19 @@ import 'package:http/http.dart';
 
 void main(List<String> args) async {
   var data = await fetchData();
-  print(data);
+  for (var d in data) {
+    print(
+        'Auteur => ${d['userId']}\nID =>  ${d['id']}\nTitre => ${d['title']}\nContenu => ${d['body']}\n');
+  }
 }
 
-Future<List<dynamic>>fetchData() async {
+Future<List<dynamic>> fetchData() async {
   final response = await get(Uri.parse(
       'https://dl.dropboxusercontent.com/scl/fi/8rqn9qjfcgq7ufwe30uut/users.json?rlkey=5heqn73zhnxepatkvt5s15d0i&st=d63ml77s&dl=0'));
+
   if (response.statusCode == 200) {
     return jsonDecode(response.body);
   } else {
-    throw Exception('Failed to fetch data');
+    throw Exception('Something went wrong!');
   }
 }
